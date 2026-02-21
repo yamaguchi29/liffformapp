e3b4a06516d10809539ae257189f93f84c58dfc3
yamaguchi29 <ya0ma5gu2chi992501@gmail.com>
2026-02-22 00:55:42 +0900

    fetch関数のURLをGAS_URLから直接スクリプトURLに変更

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
-const response = await fetch(GAS_URL, {
-  method: "POST",
-  mode: "no-cors", // これを追加するとエラーが出にくくなる場合があります（ただしレスポンスの中身は読めなくなります）
-  headers: {
-    "Content-Type": "text/plain", // GASに送る際は application/json より text/plain の方が安定することがあります
+const response = await fetch(
+  "https://script.google.com/macros/s/AKfycbwNQckWtUJ08ycrH3FCH4eslU_LG9cMKSf49ABAtX8KQ0rVcXb0vFsnLrmxAfESt1pqaA/exec",
+  {
+    method: "POST",
+    mode: "no-cors", // これを追加するとエラーが出にくくなる場合があります（ただしレスポンスの中身は読めなくなります）
+    headers: {
+      "Content-Type": "text/plain", // GASに送る際は application/json より text/plain の方が安定することがあります
+    },
+    body: JSON.stringify(data),
   },
-  body: JSON.stringify(data),
-});
+);
