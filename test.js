c119d36f0c0ab052a1b4bd545680f679aa11af75
yamaguchi29 <ya0ma5gu2chi992501@gmail.com>
2026-02-22 00:49:40 +0900

    fetch関数をGAS_URLを使用するように修正し、リクエストのヘッダーとボディを更新

--- test.js
+++ test.js
@@ -1,13 +1,8 @@
-fetch(
-  "https://script.google.com/macros/s/AKfycbwNQckWtUJ08ycrH3FCH4eslU_LG9cMKSf49ABAtX8KQ0rVcXb0vFsnLrmxAfESt1pqaA/exec",
-  {
-    method: "POST",
-    body: JSON.stringify({
-      category: "食費",
-      amount: 1200,
-      memo: "ランチ",
-    }),
+const response = await fetch(GAS_URL, {
+  method: "POST",
+  mode: "no-cors", // これを追加するとエラーが出にくくなる場合があります（ただしレスポンスの中身は読めなくなります）
+  headers: {
+    "Content-Type": "text/plain", // GASに送る際は application/json より text/plain の方が安定することがあります
   },
-)
-  .then((response) => response.json())
-  .then((data) => alert(data.message));
+  body: JSON.stringify(data),
+});
